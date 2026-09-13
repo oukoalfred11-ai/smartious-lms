35e7fde4965e642ed07d38c50ed51d9c24ff1c65
Ouko Alfred <oukoalfred11@gmail.com>
2026-09-13 15:04:41 +0300

    Update MiscModules.jsx

--- frontend/src/pages/admin/pages/modules/MiscModules.jsx
+++ frontend/src/pages/admin/pages/modules/MiscModules.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 import { useStore, api } from '../../../../context/ctx.jsx'
 import Modal from '../../../../components/ui/Modal.jsx'
 import { TOKENS } from '../shared/tokens.js'
