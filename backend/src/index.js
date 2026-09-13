629ad03656b1e6798be8895f1ba2d17f8bed781a
Ouko Alfred <oukoalfred11@gmail.com>
2026-09-13 21:40:28 +0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -108,6 +108,7 @@ app.use('/api/announcements', require('./routes/announcements'));
 app.use('/api/user-logs', require('./routes/user-logs'));
 app.use('/api/support', require('./routes/support'));
 app.use('/api/notifications', require('./routes/notifications'));
+app.use('/api/dm', require('./routes/dm'));
 app.use('/api/clubs', require('./routes/clubs'));
 app.use('/api/mastery', require('./routes/mastery'));
 app.use('/api/livekit', require('./routes/livekit'));
